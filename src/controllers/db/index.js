99be74521c886ba62213f8d62f6a07f27cf2f75b
Mashle001 <mashleburndead888@gmail.com>
2025-10-21 11:51:31 +0530

    connected to mongodb atlas

---
+++ src/controllers/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../../constatnts.js";
+
+const connectDB = async () => {
+  try {
+    const connectioninstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`Connected to MongoDB DB HOST: ${connectioninstance.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1)
+  }
+};
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constatnts.js";
+
+import connectDB from "./controllers/db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"  
+});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`)
+    console.log("Connected to MongoDB");
+
+    app.on("error", (error) => {
+      console.error("Error starting the server:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+    } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+    }
+})();
+*/
